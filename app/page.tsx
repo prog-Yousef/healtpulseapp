f9a5c1a2eefb853271897ea709a4146185937408
unknown <you@example.com>
2024-09-25 14:13:41 +0200

    fix the form and imported from schadcin

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,11 @@ import { Button } from "@/components/ui/button";
 import Link from 'next/link';
 import PatientForm from "@/components/forms/PatientForm";
 
-
 export default function Home() {
   return (
     <div className="flex h-screen max-h-screen">
+
+      {/* TODO:OTP Verification | PassKeymoudule */}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
@@ -17,19 +18,24 @@ export default function Home() {
             className="mb-12"
           />
 
-
           <PatientForm />
 
-
-          <div className="text-14-regular">
-          <p className="justify-items-end text-dark-600 xl:text-left">© 2024 CarPulse. All rights reserved.</p>
-          <Link href="/?admin=true" className="text-green-500">
-          Admin
-          </Link>
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">© 2024 CarPulse. All rights reserved.</p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
           </div>
         </div>
       </section>
+
+      <Image
+        src="/assets/images/onboarding-img.png"
+        alt="patient"
+        className="side-img max-w-[50%]"
+        width={1000}
+        height={1000}
+      />
     </div>
   );
 }
-
